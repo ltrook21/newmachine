4b86cf0ffe3160b57b8cbfdb9331ab664ee460f1
rook21 <paul.guren@gmail.com>
2022-08-11 16:59:02 -0400

    second

--- init-config.sh
+++ init-config.sh
@@ -2,8 +2,19 @@
 
 sudo apt update -y && sudo apt upgrade
 
+# backup bashrc
 sudo cp ~/.bashrc ~/.bashrc.bk
 
+
+# alias for typing
 sudo echo "alias l=\"ls -lha\"" | tee -a ~/.bashrc
 sudo echo "alias k=\"kubectl\"" | tee -a ~/.bashrc
 sudo echo "alias mk=\"minikube\"" | tee -a ~/.bashrc
+
+# install awesome vim
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+sudo echo "set numbers" | tee -a ~/.vim_runtime/my_configs.vim
+
+# config nano
+sudo cp ./.nanorc ~ 
