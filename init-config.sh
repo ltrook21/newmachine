7ad45de042119c061e50c98f170c4ce3282353b9
Paulguren <rook@windesk22.localdomain>
2022-08-13 16:45:56 -0400

    updates :)

--- init-config.sh
+++ init-config.sh
@@ -10,6 +10,13 @@ sudo cp ~/.bashrc ~/.bashrc.bk
 sudo echo "alias l=\"ls -lha\"" | tee -a ~/.bashrc
 sudo echo "alias k=\"kubectl\"" | tee -a ~/.bashrc
 sudo echo "alias mk=\"minikube\"" | tee -a ~/.bashrc
+sudo echo "alias dk=\"docker\"" | tee -a ~/.bashrc
+sudo echo "alias dkc=\"docker compose\"" | tee -a ~/.bashrc
+
+sudo echo "alias k=\"kubectl\"" | tee -a ~/.zshrc
+sudo echo "alias mk=\"minikube\"" | tee -a ~/.zshrc
+sudo echo "alias dk=\"docker\"" | tee -a ~/.zshrc
+sudo echo "alias dkc=\"docker compose\"" | tee -a ~/.zshrc
 
 # install awesome vim
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
@@ -17,4 +24,4 @@ sh ~/.vim_runtime/install_awesome_vimrc.sh
 sudo echo "set numbers" | tee -a ~/.vim_runtime/my_configs.vim
 
 # config nano
-sudo cp ./.nanorc ~ 
+sudo cp ./.nanorc ~
