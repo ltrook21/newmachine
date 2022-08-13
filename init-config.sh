7efcc0d92025b3a54baf72dd54413d3989d9617a
Paulguren <rook@windesk22.localdomain>
2022-08-13 16:51:51 -0400

    updates :)2

--- init-config.sh
+++ init-config.sh
@@ -5,6 +5,16 @@ sudo apt update -y && sudo apt upgrade
 # backup bashrc
 sudo cp ~/.bashrc ~/.bashrc.bk
 
+# install zsh
+sudo apt install zsh
+
+# config oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install p10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
 
 # alias for typing
 sudo echo "alias l=\"ls -lha\"" | tee -a ~/.bashrc
