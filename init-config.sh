93348b7c554f44dc6d53f2cb8c8967962c1b5b1f
rook21 <rook21@razwin22.localdomain>
2022-08-18 19:48:52 -0400

    update

--- init-config.sh
+++ init-config.sh
@@ -22,11 +22,13 @@ echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 # alias for typing
 sudo echo "alias l=\"ls -lha\"" | tee -a ~/.bashrc
 sudo echo "alias k=\"kubectl\"" | tee -a ~/.bashrc
+sudo echo "alias kx=\"kubectx\"" | tee -a ~/.bashrc
 sudo echo "alias mk=\"minikube\"" | tee -a ~/.bashrc
 sudo echo "alias dk=\"docker\"" | tee -a ~/.bashrc
 sudo echo "alias dkc=\"docker compose\"" | tee -a ~/.bashrc
 
 sudo echo "alias k=\"kubectl\"" | tee -a ~/.zshrc
+sudo echo "alias kx=\"kubectx\"" | tee -a ~/.zshrc
 sudo echo "alias mk=\"minikube\"" | tee -a ~/.zshrc
 sudo echo "alias dk=\"docker\"" | tee -a ~/.zshrc
 sudo echo "alias dkc=\"docker compose\"" | tee -a ~/.zshrc
