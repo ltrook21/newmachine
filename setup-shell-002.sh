99a962a96a04bd9862a5d46b876007f002ec6dda
rook21 <paul.guren@gmail.com>
2022-08-18 23:32:02 -0400

    new structure

---
+++ setup-shell-002.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Shortcuts bash + zsh
+sudo echo "alias l=\"ls -lha\"" | tee -a ~/.bashrc
+sudo echo "alias k=\"kubectl\"" | tee -a ~/.bashrc
+sudo echo "alias mk=\"minikube\"" | tee -a ~/.bashrc
+sudo echo "alias dk=\"docker\"" | tee -a ~/.bashrc
+sudo echo "alias dkc=\"docker compose\"" | tee -a ~/.bashrc
+sudo echo "alias k=\"kubectl\"" | tee -a ~/.zshrc
+sudo echo "alias mk=\"minikube\"" | tee -a ~/.zshrc
+sudo echo "alias dk=\"docker\"" | tee -a ~/.zshrc
+sudo echo "alias dkc=\"docker compose\"" | tee -a ~/.zshrc
---
+++ setup.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "Running initial scripts..."
+
+echo "001 running..."
+sudo bash setup-shell-001.sh
+
+echo "002 running..."
+sudo bash setup-shell-002.sh
