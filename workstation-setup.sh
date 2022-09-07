baad981433079e5c9941b344955a3fc05f180333
rook21 <paul.guren@gmail.com>
2022-09-07 15:09:20 -0400

    update

---
+++ workstation-setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+sudo apt update -y && sudo apt upgrade -y
+
+
+## DOCKER
+echo "\n\nDOCKER\n\n"
+# set up docker repo
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+
+# add docker gpg key
+sudo mkdir -p /etc/apt/keyrings -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+
+# set up docker repo
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+
+## MINIKUBE
+echo "\n\nMINIKUBE\n\n"
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube -y
+
+
+## KUBECTX
+echo "\n\nKUBECTX\n\n"
+sudo apt install kubectx -y
