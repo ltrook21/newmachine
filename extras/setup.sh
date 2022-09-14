ca49ac3f2b8a623f98ff60506895e476cca54359
rook21 <paul.guren@gmail.com>
2022-09-13 22:08:52 -0400

    Fixed structure pt 2

--- extras/setup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-echo "\n\nRunning initial scripts...\n\n"
-
-echo "\n\n001 running...\n\n"
-sudo bash setup-shell-001.sh
-
-echo "\n\n002 running...\n\n"
-sudo bash setup-shell-002.sh
