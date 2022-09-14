e5b9ff353c7a454604257e78f3ad37f17335f24b
rook21 <paul.guren@gmail.com>
2022-09-13 22:04:48 -0400

    Cleaned up structure

---
+++ init-terminal-config.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+sudo apt update -y && sudo apt upgrade
+
+# This is for theming only. Moved to different file
+# sudo apt install breeze-cursor-theme
+
+# backup bashrc
+sudo cp ~/.bashrc ~/.bashrc.bk
+
+# install zsh
+sudo apt install zsh
+
+# config oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# im still not sure about lines 15 and 16.
+# Reset zsh for new config:
+# exec zsh
+
+# install p10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+# alias for typing
+sudo echo "alias l=\"ls -lha\"" | tee -a ~/.bashrc
+sudo echo "alias k=\"kubectl\"" | tee -a ~/.bashrc
+sudo echo "alias kx=\"kubectx\"" | tee -a ~/.bashrc
+sudo echo "alias mk=\"minikube\"" | tee -a ~/.bashrc
+sudo echo "alias dk=\"docker\"" | tee -a ~/.bashrc
+sudo echo "alias dkc=\"docker compose\"" | tee -a ~/.bashrc
+
+sudo echo "alias k=\"kubectl\"" | tee -a ~/.zshrc
+sudo echo "alias kx=\"kubectx\"" | tee -a ~/.zshrc
+sudo echo "alias mk=\"minikube\"" | tee -a ~/.zshrc
+sudo echo "alias dk=\"docker\"" | tee -a ~/.zshrc
+sudo echo "alias dkc=\"docker compose\"" | tee -a ~/.zshrc
+
+# install awesome vim
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+sudo echo "set number" | tee -a ~/.vim_runtime/my_configs.vim
+sudo echo "colorscheme dracula" | tee -a ~/.vim_runtime/my_configs.vim
+
+# config nano
+sudo cp ./.nanorc ~
+
+exec zsh
---
+++ new-instance/startup-aws-userdata.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+sudo apt update -y && sudo apt upgrade -y
+
+
+## DOCKER
+
+# set up docker repo
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+
+# add docker gpg key
+sudo mkdir -p /etc/apt/keyrings -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+
+# set up docker repo
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+
+## MINIKUBE
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube -y
+
+
+## KUBECTX
+
+sudo apt install kubectx -y
