ea946792bc6fe2dcc9c6331f0385c532b9e568b3
rook21 <paul.guren@gmail.com>
2022-09-13 22:21:47 -0400

    Fixed structure pt 3

--- workstation-setup.sh
+++ workstation-setup.sh
@@ -32,6 +32,12 @@ echo "\n\nMINIKUBE\n\n"
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
+
+## KUBECTL
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+
 ## KUBECTX
 echo \n "KUBECTX"\n
 # sudo apt install -y kubectx
