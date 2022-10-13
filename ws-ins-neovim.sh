a054f8294747ea2465020418f262abc129e15a6f
rook21 <paul.guren@gmail.com>
2022-10-13 00:30:27 -0400

    update

--- ws-ins-neovim.sh
+++ ws-ins-neovim.sh
@@ -21,3 +21,19 @@ sudo cp ~/scripts/newmachine/init.vim ~/.config/nvim -y
 # vim plug for NeoVim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
+# install nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash -y
+source ~/.bashrc
+
+sudo nvm install v16.18.0 -y
+
+sudo apt install npm -y
+
+cd ~/.config/nvim/coc.nvim
+
+sudo npm install -g yarn -y
+
+yarn install
+yarn build
