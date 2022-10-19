8e114e469a3214ebe686b0badfb98de68b2cb803
rook21 <paul.guren@gmail.com>
2022-10-19 15:00:23 -0400

    update

--- aliases.sh
+++ aliases.sh
@@ -41,7 +41,7 @@ sudo echo "alias nv=\"nvim\"" | tee -a ~/.zshrc
 sudo echo "alias kg=\"k get\"" | tee -a ~/.zshrc
 sudo echo "alias kgp=\"k get po\"" | tee -a ~/.zshrc
 sudo echo "alias kgn=\"k get no\"" | tee -a ~/.zshrc
-sudo echo "alias kgl=\"k get log\"" | tee -a ~/.zshrc
+sudo echo "alias kgl=\"k get logs\"" | tee -a ~/.zshrc
 sudo echo "alias kghpa=\"k get hpa\"" | tee -a ~/.zshrc
 sudo echo "alias kgs=\"k get svc\"" | tee -a ~/.zshrc
 sudo echo "alias kgd=\"k get deploy\"" | tee -a ~/.zshrc
@@ -51,9 +51,16 @@ sudo echo "alias ka=\"k apply -f \"" | tee -a ~/.zshrc
 sudo echo "alias kpf=\"k port-forward\"" | tee -a ~/.zshrc
 sudo echo "alias kds=\"k describe\"" | tee -a ~/.zshrc
 sudo echo "alias kd=\"k delete\"" | tee -a ~/.zshrc
+sudo echo "alias kns=\"kubens\"" | tee -a ~/.zshrc
 
+
+
+# python
 sudo echo "alias venv=\"source venv/bin/activate\"" | tee -a ~/.zshrc
 sudo echo "alias pmr=\"python manage.py runserver\"" | tee -a ~/.zshrc
+sudo echo "alias f=\"flask\"" | tee -a ~/.zshrc
+
+# git
 sudo echo "alias gtc=\"git commit -a\"" | tee -a ~/.zshrc
 sudo echo "alias gtp=\"git push\"" | tee -a ~/.zshrc
 sudo echo "alias gtpu=\"git pull\"" | tee -a ~/.zshrc
