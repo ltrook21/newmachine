34474255a7a92cd41423089675fd476fe02bb6ae
rook <paul.guren@gmail.com>
2023-09-12 23:48:40 -0400

    fixed

--- golang/main.go
+++ golang/main.go
@@ -2,76 +2,74 @@ package main
 
 import (
 	"fmt"
-//	"io/ioutil"
+	//	"io/ioutil"
 	"os"
 	"path/filepath"
 )
 
 const bashcontentToAdd = `
-sudo echo "alias l=\"ls -lha\"" | tee -a ~/.bashrc
-sudo echo "alias k=\"kubectl\"" | tee -a ~/.bashrc
-sudo echo "alias kx=\"kubectx\"" | tee -a ~/.bashrc
-sudo echo "alias mk=\"minikube\"" | tee -a ~/.bashrc
-sudo echo "alias dk=\"docker\"" | tee -a ~/.bashrc
-sudo echo "alias dkc=\"docker compose\"" | tee -a ~/.bashrc
-sudo echo "alias nv=\"nvim\"" | tee -a ~/.bashrc
+export alias l="ls -lha"
+export alias k="kubectl"
+export alias kx="kubectx"
+export alias mk="minikube"
+export alias dk="docker"
+export alias dkc="docker compose"
+export alias nv="nvim"
 
 
-sudo echo "alias kgp=\"k get po\"" | tee -a ~/.bashrc
-sudo echo "alias kgs=\"k get svc\"" | tee -a ~/.bashrc
-sudo echo "alias kg=\"k get\"" | tee -a ~/.bashrc
-sudo echo "alias kgd=\"k get deploy\"" | tee -a ~/.bashrc
-sudo echo "alias kc=\"k create\"" | tee -a ~/.bashrc
-sudo echo "alias kr=\"k run\"" | tee -a ~/.bashrc
-sudo echo "alias ka=\"k apply -f \"" | tee -a ~/.bashrc
-sudo echo "alias kpf=\"k port-forward\"" | tee -a ~/.bashrc
-sudo echo "alias kds=\"k describe\"" | tee -a ~/.bashrc
-sudo echo "alias kd=\"k delete\"" | tee -a ~/.bashrc
+export alias kgp="k get po"
+export alias kgs="k get svc"
+export alias kg="k get"
+export alias kgd="k get deploy"
+export alias kc="k create"
+export alias kr="k run"
+export alias ka="k apply -f "
+export alias kpf="k port-forward"
+export alias kds="k describe"
+export alias kd="k delete"
 `
 
-
-
 const zshcontentToAdd = `
-sudo echo "alias k=\"kubectl\"" | tee -a ~/.zshrc
-sudo echo "alias kx=\"kubectx\"" | tee -a ~/.zshrc
-sudo echo "alias mk=\"minikube\"" | tee -a ~/.zshrc
-sudo echo "alias dk=\"docker\"" | tee -a ~/.zshrc
-sudo echo "alias dkc=\"docker compose\"" | tee -a ~/.zshrc
-sudo echo "alias nv=\"nvim\"" | tee -a ~/.zshrc
+export alias k="kubectl"
+export alias kx="kubectx"
+export alias mk="minikube"
+export alias dk="docker"
+export alias dkc="docker compose"
+export alias nv="nvim"
 
 # k8s
 
-sudo echo "alias kg=\"k get\"" | tee -a ~/.zshrc
-sudo echo "alias kgp=\"k get po\"" | tee -a ~/.zshrc
-sudo echo "alias kgn=\"k get no\"" | tee -a ~/.zshrc
-sudo echo "alias kgl=\"k get logs\"" | tee -a ~/.zshrc
-sudo echo "alias kghpa=\"k get hpa\"" | tee -a ~/.zshrc
-sudo echo "alias kgs=\"k get svc\"" | tee -a ~/.zshrc
-sudo echo "alias kgd=\"k get deploy\"" | tee -a ~/.zshrc
-sudo echo "alias kc=\"k create\"" | tee -a ~/.zshrc
-sudo echo "alias kr=\"k run\"" | tee -a ~/.zshrc
-sudo echo "alias ka=\"k apply -f \"" | tee -a ~/.zshrc
-sudo echo "alias kpf=\"k port-forward\"" | tee -a ~/.zshrc
-sudo echo "alias kds=\"k describe\"" | tee -a ~/.zshrc
-sudo echo "alias kd=\"k delete\"" | tee -a ~/.zshrc
-sudo echo "alias kns=\"kubens\"" | tee -a ~/.zshrc
+export alias kg="k get"
+export alias kgp="k get po"
+export alias kgn="k get no"
+export alias kgl="k get logs"
+export alias kghpa="k get hpa"
+export alias kgs="k get svc"
+export alias kgd="k get deploy"
+export alias kc="k create"
+export alias kr="k run"
+export alias ka="k apply -f "
+export alias kpf="k port-forward"
+export alias kds="k describe"
+export alias kd="k delete"
+export alias kns="kubens"
 
 
 # python
-sudo echo "alias venv=\"source venv/bin/activate\"" | tee -a ~/.zshrc
-sudo echo "alias pmr=\"python manage.py runserver\"" | tee -a ~/.zshrc
-sudo echo "alias f=\"flask\"" | tee -a ~/.zshrc
-sudo echo "alias ve=\"source venv/bin/activate\"" | tee -a ~/.zshrc
-sudo echo "alias p=\"python\"" | tee -a ~/.zshrc
+export alias venv="source venv/bin/activate"
+export alias pmr="python manage.py runserver"
+export alias f="flask"
+export alias ve="source venv/bin/activate"
+export alias p="python"
 
 # git
-sudo echo "alias gtc=\"git commit -a\"" | tee -a ~/.zshrc
-sudo echo "alias gtp=\"git push\"" | tee -a ~/.zshrc
-sudo echo "alias gtpu=\"git pull\"" | tee -a ~/.zshrc
+export alias gtc="git commit -a"
+export alias gtp="git push"
+export alias gtpu="git pull"
 
 
 # vpn
-sudo echo "alias pvc=\"protonvpn-cli\"" | tee -a ~/.zshrc
+export alias pvc="protonvpn-cli"
 `
 
 func appendToFile(path string, content string) error {
@@ -114,6 +112,3 @@ func main() {
 		fmt.Println("Appended content to .bashrc!")
 	}
 }
-
-
-
