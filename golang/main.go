1544b2e1c6fb70ba9320243e82ed8eedb86f2787
Paul <paul.guren@gmail.com>
2023-09-22 10:33:26 -0400

    Cleanup (#1)
    * rewrite in golang
    
    * rewrite in golanggit status
    
    * removed theme files and just left a note for which themes to download
    
    * added go binaries that add the shortcuts to terminal
    
    * fixed
    
    * fixed typo
    
    * removed deprecated binaries
    
    * commit nvim config

---
+++ golang/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	//	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+const bashcontentToAdd = `
+alias l="ls -lha"
+alias k="kubectl"
+alias kx="kubectx"
+alias mk="minikube"
+alias dk="docker"
+alias dkc="docker compose"
+alias nv="nvim"
+
+
+alias kgp="k get po"
+alias kgs="k get svc"
+alias kg="k get"
+alias kgd="k get deploy"
+alias kc="k create"
+alias kr="k run"
+alias ka="k apply -f "
+alias kpf="k port-forward"
+alias kds="k describe"
+alias kd="k delete"
+`
+
+const zshcontentToAdd = `
+alias k="kubectl"
+alias kx="kubectx"
+alias mk="minikube"
+alias dk="docker"
+alias dkc="docker compose"
+alias nv="nvim"
+
+# k8s
+
+alias kg="k get"
+alias kgp="k get po"
+alias kgn="k get no"
+alias kgl="k get logs"
+alias kghpa="k get hpa"
+alias kgs="k get svc"
+alias kgd="k get deploy"
+alias kc="k create"
+alias kr="k run"
+alias ka="k apply -f "
+alias kpf="k port-forward"
+alias kds="k describe"
+alias kd="k delete"
+alias kns="kubens"
+
+
+# python
+alias venv="source venv/bin/activate"
+alias pmr="python manage.py runserver"
+alias f="flask"
+alias ve="source venv/bin/activate"
+alias p="python"
+
+# git
+alias gtc="git commit -a"
+alias gtp="git push"
+alias gtpu="git pull"
+
+
+# vpn
+alias pvc="protonvpn-cli"
+`
+
+func appendToFile(path string, content string) error {
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err := f.WriteString(content); err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	zshrcPath := filepath.Join(home, ".zshrc")
+	bashrcPath := filepath.Join(home, ".bashrc")
+
+	// Check if ~/.zshrc exists
+	if _, err := os.Stat(zshrcPath); !os.IsNotExist(err) {
+		// Append to ~/.zshrc
+		if err := appendToFile(zshrcPath, zshcontentToAdd); err != nil {
+			fmt.Println("Error appending to .zshrc:", err)
+			return
+		}
+		fmt.Println("Appended content to .zshrc!")
+	} else {
+		// Append to ~/.bashrc
+		if err := appendToFile(bashrcPath, bashcontentToAdd); err != nil {
+			fmt.Println("Error appending to .bashrc:", err)
+			return
+		}
+		fmt.Println("Appended content to .bashrc!")
+	}
+}
