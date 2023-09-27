0634c160c832ef12cfebca957c400ddd10f0a025
Paul Aranguren <paul.guren@gmail.com>
2023-09-26 23:10:17 -0400

    added nvchad config 2

--- nvim/nvchad/lua/custom/configs/null-ls.lua
+++ nvim/nvchad/lua/custom/configs/null-ls.lua
@@ -4,6 +4,7 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local opts = {
   sources = {
     null_ls.builtins.formatting.gofmt,
+    null_ls.builtins.formatting.goimports_reviser,
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
