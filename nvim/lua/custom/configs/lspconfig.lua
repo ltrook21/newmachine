f734f681ab1b099f785e84ef2f3aa12ebad7a501
Paul Aranguren <paul.guren@gmail.com>
2023-10-25 11:42:20 -0400

    update

---
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,37 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require("lspconfig")
+local util = require "lspconfig/util"
+
+
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"rust"},
+  root_dir = lspconfig.util.root_pattern("Cargo.toml"),
+})
+
+lspconfig.gopls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = {"gopls"},
+  filetypes = {"go", "gomod", "gowork", "gotmpl"},
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+      }
+    }
+  }
+})
+
+lspconfig.marksman.setup({
+  cmd = { "marksman", "server" },
+  filetypes = { "markdown", "markdown.mdx" },
+  root_dir = util.root_pattern(".git", ".marksman.toml"),
+  single_file_support = true,
+})
---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,35 @@
+local plugins = {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+    require "plugins.configs.lspconfig"
+    require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+      "rust-analyzer",
+      "gopls",
+      "yaml-configuration-server",
+      "dockerfile-configuration-server",
+      "docker-compose-language-server",
+      },
+    },
+    {
+      "rust-lang/rust.vim",
+      ft = "rust",
+      init = function ()
+        vim.g.rustfmt_autosave = 1
+      end
+    },
+    {
+      "jose-elias-alvarez/null-ls.nvim",
+    },
+    {
+      require'lspconfig'.marksman.setup{}
+    },
+  }
+}
+return plugins
