8580765da654092f0610b90da95da0ce303663b6
Paul Aranguren <paul.aranguren@imaginelearning.com>
2023-11-08 15:51:54 -0500

    added quit with q to nvim

--- nvim/nvchad/lua/custom/init.lua
+++ nvim/nvchad/lua/custom/init.lua
@@ -1,2 +1,9 @@
 -- Map Ctrl + S to save the file in insert mode
 vim.api.nvim_set_keymap('i', '<C-S>', '<Esc>:w<CR>i', { noremap = true })
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Quit Neovim with a single 'Q' press in normal mode
+map('n', 'Q', ':qa<CR>', opts)
+
