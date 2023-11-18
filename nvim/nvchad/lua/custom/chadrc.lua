4d1757a3d9b38e96ef9482a3d9f225672f1ee823
Paul Aranguren <paul.guren@gmail.com>
2023-11-18 12:51:28 -0500

    added nvchad config :)

--- nvim/nvchad/lua/custom/chadrc.lua
+++ nvim/nvchad/lua/custom/chadrc.lua
@@ -1,5 +1,31 @@
 ---@type ChadrcConfig
 local M = {}
-M.ui = { theme = 'yoru' }
+M.ui = { 
+  theme = 'yoru',
+  theme_toggle = {'yoru', "one_light"},
+  transparency = true,
+  cmp = {
+    icons = true,
+  },
+  statusline = {
+    theme = "vscode",
+    separator_style = "round",
+  },
+  tabufline = {},
+  nvdash = {
+    load_on_startup = true,
+    header = {
+      " WELCOME, ROOK "
+    },
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  }
+}
 M.plugins = 'custom.plugins'
 return M
