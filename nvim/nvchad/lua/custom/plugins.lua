99c9c9277d54a223e736bf74ed1896e3eb0b54b9
rook21 <paul.guren@gmail.com>
2024-01-17 03:18:01 +0000

    autosave

--- nvim/nvchad/lua/custom/plugins.lua
+++ nvim/nvchad/lua/custom/plugins.lua
@@ -37,6 +37,48 @@ local plugins = {
     "ellisonleao/glow.nvim", config = true, cmd = "Glow"
   },
   {
+  'rmagatti/auto-session',
+  config = function()
+    require("auto-session").setup {
+      log_level = vim.log.levels.ERROR,
+      auto_session_root_dir = '~/.config/nvim/session',
+      auto_session_allowed_dirs = '~/.config/nvim/session',
+      auto_session_enable_last_session = true,
+      auto_session_enabled = true,
+      auto_session_create_enabled = true,
+      auto_save_enabled = true,
+      auto_restore_enabled = true,
+      auto_session_session_name = 'Session.vim', -- Set the session file name (optional)
+
+      -- The following are already the default values, no need to provide them if these are already the settings you want.
+      session_lens = {
+        load_on_setup = true,
+        theme_conf = { border = true },
+        previewer = false,
+      },
+    }
+  end
+  },
+  {
+  "Pocco81/auto-save.nvim",
+  config = function()
+    require("auto-save").setup {
+      enabled = true,
+      execution_message = "AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"),
+      events = {"InsertLeave", "TextChanged"},
+      conditions = {
+        exists = true,
+        filetype_is_not = {},
+        modifiable = true
+      },
+      write_all_buffers = false,
+      on_off_commands = true,
+      clean_command_line_interval = 0,
+      debounce_delay = 135
+    }
+  end
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
